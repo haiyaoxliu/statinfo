50c58cb748b6df3193e360fcc7de951224704c9d
hliu3 <hliu3@stuy.edu>
2017-10-31 22:35:34 -0400

    size formatting done

--- statinfo.c
+++ statinfo.c
@@ -4,21 +4,42 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
 
-int pinfo(char* path) {
-  //int s, m;
-  //long t;
+int ginfo(char* path) {
+
+  //init
   struct stat info;
-  if(stat(path,&info) < 0) {
-    return 1;
-  }
   char size[256];
-  
-  sprintf(size,"%lld",info.st_size);
-  printf("Information:\n\tpath:\t%s\n\tfile size:\t%s\n\tmode:\t%d\n\tlast access:\t%s\n",path,size,info.st_mode,ctime(&(info.st_atime)));
+  int mode;
+
+  //error
+  if(stat(path,&info) < 0) {
+     return 1;
+  }
+  //formatting
+  //size
+  static const char *abbr[] = { "B", "KB", "MB", "GB" };
+  size_t d = 0;
+  long s = info.st_size;
+
+  while (s >= 1024 && d < (sizeof abbr / sizeof *abbr)) {
+    d++;   
+    s /= 1024;
+  }
+  sprintf(size, "%ld %s",s,abbr[d]);
+
+  //mode
+  mode = 0000666 & info.st_mode;
+
+  printf("\n\tpath:\t\t%s\n\tfile size:\t%s\n\tmode:\t\t%o\n\tlast access:\t%s\n",path,size,mode,ctime(&(info.st_atime)));
   return 0;
 }
 
 int main() {
-  pinfo("./t.txt");
+
+  char path[] = "./t.txt";
+  ginfo(path); //? printf("%s",info) : printf("\nERROR file at:\n\t%s\nnot found\n",path);
+
 }
