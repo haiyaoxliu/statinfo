0b9aff264f9ebda55d57b630fe84e42888d93d0c
hliu3 <hliu3@stuy.edu>
2017-11-01 10:22:34 -0400

    some updates

--- statinfo.c
+++ statinfo.c
@@ -7,38 +7,55 @@
 #include <math.h>
 #include <string.h>
 
+int efile(char* path) {
+  int fd;
+  //open(fd, ,);
+
+  return 0;
+}
+
 int ginfo(char* path) {
 
-  //init
-  struct stat info;
-  char size[256];
-  int mode;
-
   //error
+  struct stat info;
   if(stat(path,&info) < 0) {
      return 1;
   }
-  //formatting
-  //size
-  static const char *abbr[] = { "B", "KB", "MB", "GB" };
-  size_t d = 0;
-  long s = info.st_size;
 
-  while (s >= 1024 && d < (sizeof abbr / sizeof *abbr)) {
-    d++;   
-    s /= 1024;
+  //decl & init
+  char *abbr[] = { "B", "kB", "MB", "GB", "TB" }; //static const
+  char *mode[] = { "r", "w", "x", "-" };
+  char *time = ctime(&(info.st_atime));
+
+  size_t unit = 0;          //size unit tracker
+  long len = info.st_size;  //raw size data
+  int pos = 0;              //mode pos. tracker
+  int nmode = info.st_mode; //raw mode data
+
+  char size[8];             //format: ABCD { ,K,M,G}B\0, 8 chars minimum
+  char mode[10];            //rwxr--r--, 10 chars minimum
+
+  //size formatting
+  while (len >= 1024 && unit < (sizeof abbr / sizeof *abbr)) {
+    unit++;
+    len /= 1024;
   }
-  sprintf(size, "%ld %s",s,abbr[d]);
+  sprintf(size, "%ld %s",len,abbr[unit]);
 
-  //mode
-  mode = 0000666 & info.st_mode;
+  //mode formatting
+  nmode &= 0000666;
+  
 
-  printf("\n\tpath:\t\t%s\n\tfile size:\t%s\n\tmode:\t\t%o\n\tlast access:\t%s\n",path,size,mode,ctime(&(info.st_atime)));
+  printf("\n\tpath:\t\t%s",path);
+  printf("\n\tfile size:\t%s",size);
+  printf("\n\tmode:\t\t%b",mode);
+  printf("\n\tlast access:\t%s\n",time);
   return 0;
 }
 
 int main() {
 
+  efile("t.txt");
   char path[] = "./t.txt";
   ginfo(path); //? printf("%s",info) : printf("\nERROR file at:\n\t%s\nnot found\n",path);
 
